d4c0e74e609f4eb33e09f4e1969cf14ead6778a8
Sachin Rajput <srajput1@binghamton.edu>
2014-01-03 14:06:47 -0500

    Changed popstack function.

--- stack/stack.c
+++ stack/stack.c
@@ -3,44 +3,50 @@
 /***
  * Creates a stack using structure containing a dlinklist
  */
-stack* createStack(){
-        stack* new_stack = (stack*)malloc(sizeof(stack)); 
-        new_stack->ll = createlinklist();
-        return new_stack;
+stack* createStack()
+{
+	stack* new_stack = (stack*)malloc(sizeof(stack));
+	new_stack->ll = createlinklist();
+	return new_stack;
 }
 
 /***
- * Pushes the data in the stack from top 
+ * Pushes the data in the stack from top
  */
-void stackPush(stack *s,data *d){
-        addBack(s->ll,d);
+void stackPush(stack *s,data *d)
+{
+	addBack(s->ll,d);
 }
 
 /***
  * Pop's data from the top
  */
-data* stackPop(stack *s){
-        return popBack(s->ll);
+void stackPop(stack *s)
+{
+	popBack(s->ll);
 }
 
 /***
  * Print the data from the stack
  */
-void stackPrint(stack *s){
-        printList(s->ll);
+void stackPrint(stack *s)
+{
+	printList(s->ll);
 }
 
 /***
  * Get the stack size
  */
-int stackSize(stack *s){
-        return listSize(s->ll);
+int stackSize(stack *s)
+{
+	return listSize(s->ll);
 }
 
 /***
  * Cleans the stack
  */
-void stackClean(stack *s){
-        cleanList(s->ll);
-		free(s);
+void stackClean(stack *s)
+{
+	cleanList(s->ll);
+	free(s);
 }
